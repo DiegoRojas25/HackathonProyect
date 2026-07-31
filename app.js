3df546ca003dfafac4cd1386da77e755f9624343
JorgeGutierrezVallejo <jegv08@gmail.com>
2026-07-31 17:03:58 -0500

    Agrega productos, imágenes, filtro

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const productos = [
         precio: 22000,
         imagen: "img/tacos.jpg",
         descripcion: "Carne de cerdo marinada con piña.",
-        categoria:"platos fuertes."
+        categoria: "platos"
     },
     {
         id: 2,
@@ -13,7 +13,7 @@ const productos = [
         precio: 26000,
         imagen: "img/burrito.jpg",
         descripcion: "Res, arroz, frijoles y queso.",
-        categoria:"platos fuertes."
+        categoria: "platos"
     },
     {
         id: 3,
@@ -21,9 +21,56 @@ const productos = [
         precio: 18000,
         imagen: "img/quesadilla.jpg",
         descripcion: "Tortilla de maíz con queso.",
-        categoria:"platos fuertes."
-    }
-
+        categoria: "platos"
+    },
+   {
+    id: 4,
+    nombre: "Nachos con Guacamole",
+    precio: 16000,
+    imagen: "img/nachos-guacamole.png",
+    descripcion: "Nachos crujientes acompañados de guacamole.",
+    categoria: "entradas"
+},
+{
+    id: 5,
+    nombre: "Elote Mexicano",
+    precio: 12000,
+    imagen: "img/elote-mexicano.png",
+    descripcion: "Mazorca con queso, limón y un toque picante.",
+    categoria: "entradas"
+},
+{
+    id: 6,
+    nombre: "Jalapeños Rellenos",
+    precio: 15000,
+    imagen: "img/jalapenos-rellenos.png",
+    descripcion: "Jalapeños rellenos de queso y empanizados.",
+    categoria: "entradas"
+},
+{
+    id: 7,
+    nombre: "Agua de Horchata",
+    precio: 8000,
+    imagen: "img/horchata.png",
+    descripcion: "Bebida de arroz con canela, fresca y cremosa.",
+    categoria: "bebidas"
+},
+{
+    id: 8,
+    nombre: "Agua de Jamaica",
+    precio: 7000,
+    imagen: "img/agua-jamaica.png",
+    descripcion: "Bebida refrescante de flor de Jamaica.",
+    categoria: "bebidas"
+},
+{
+    id: 9,
+    nombre: "Mangonada",
+    precio: 12000,
+    imagen: "img/mangonada.png",
+    descripcion: "Mango natural con chamoy, limón y tajín.",
+    categoria: "bebidas"
+}
 ];
 
 const catalogo = document.getElementById("catalogoProductos");
@@ -31,7 +78,7 @@ const catalogo = document.getElementById("catalogoProductos");
 function mostrarProductos() {
     productos.forEach(producto => {
         catalogo.innerHTML += `
-            <div class="${producto.categoria} col-md-4">
+            <div class="producto-item col-md-4" data-categoria="${producto.categoria}">
                 <div class="cards-platos card h-100">
 
                     <img src="${producto.imagen}" class="card-img-top">
@@ -69,3 +116,34 @@ function saleCarrito() {
 function guardarCarrito() {
     overlay.classList.add("d-none")
 }
+
+
+
+const botonesFiltroCategorias = document.querySelectorAll(
+    ".btn-categoria, .btn-menu-completo"
+);
+
+function filtrarProductosPorCategoria(categoriaSeleccionada) {
+    const tarjetasProductos = catalogo.querySelectorAll(".producto-item");
+
+    tarjetasProductos.forEach(tarjeta => {
+        const categoriaProducto = tarjeta.dataset.categoria;
+
+        if (
+            categoriaSeleccionada === "todos" ||
+            categoriaProducto === categoriaSeleccionada
+        ) {
+            tarjeta.classList.remove("d-none");
+        } else {
+            tarjeta.classList.add("d-none");
+        }
+    });
+}
+
+botonesFiltroCategorias.forEach(boton => {
+    boton.addEventListener("click", function () {
+        const categoriaSeleccionada = boton.dataset.categoria;
+
+        filtrarProductosPorCategoria(categoriaSeleccionada);
+    });
+});
