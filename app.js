d5e8105395b3d0212a6a7c941b3e7d382c7e62fe
SimonF <simonfrancoguisado@gmail.com>
2026-07-31 12:57:08 -0500

    Subiendo la navbar y el contenedor del carrito de compra

---
+++ app.js
@@ -0,0 +1,14 @@
+const mostrarCarrito = document.getElementById("mostrarCarrito");
+const overlay = document.getElementById("overlay");
+const cerrarCarrito = document.getElementById("cerrarCarrito");
+
+cerrarCarrito.addEventListener("click", guardarCarrito);
+mostrarCarrito.addEventListener("click", saleCarrito);
+
+function saleCarrito() {
+    overlay.classList.remove("d-none")
+}
+
+function guardarCarrito() {
+    overlay.classList.add("d-none")
+}
